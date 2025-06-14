6b5c8e5c0f489bc725af1651cb444a0997db0f1d
PWR <pwrpntr@gmail.com>
2025-06-14 21:01:02 +0200

    update portable text render

--- src/lib/modules/sanity/index.ts
+++ src/lib/modules/sanity/index.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@sanity/client"
-import blocksToHtml from '@sanity/block-content-to-html'
+import { toHTML } from '@portabletext/to-html'
 import imageUrlBuilder from '@sanity/image-url'
-import type { BlockContent } from "$lib/types/sanity.types";
+import type { PortableTextBlock } from '@portabletext/types'
 import { SANITY_ID } from "$lib/constants";
 import { PUBLIC_ENVIRONMENT } from "$env/static/public"
 
@@ -10,61 +10,46 @@ export const client = createClient({
     dataset: 'production',
     token: '', // or leave blank to be anonymous user
     useCdn: PUBLIC_ENVIRONMENT ===  "preview" ? false : true, // Don't use the CDN for preview
-    apiVersion: '2024-06-06',
+    apiVersion: '2025-06-01',
 })
 
-const h = blocksToHtml.h
-
-export const renderBlockText = (blocks: BlockContent) =>
-    blocksToHtml({
-        blocks: blocks,
-        serializers: serializers,
-        projectId: SANITY_ID,
-        dataset: 'production',
-    })
-
-export const toPlainText = (blocks: BlockContent) => {
-    return (
-        blocks
-            .map(block => {
-                if (block._type !== 'block' || !block.children) {
-                    return ''
+export const renderBlockText = (blocks: PortableTextBlock[]) => {
+    return toHTML(blocks, {
+        components: {
+            marks: {
+                link: ({ children, value }) => {
+                    return `<a href="${value.href}" target="_blank" rel="noreferrer">${children}</a>`
                 }
-                return block.children.map(child => child.text).join('')
-            })
-            .join('\n\n')
-    )
+            },
+            block: {
+                normal: ({ children }) => `<p>${children}</p>`,
+                blockquote: ({ children }) => `<blockquote>${children}</blockquote>`,
+            },
+            types: {
+                imageGroup: ({ value }) => `<p>${value}</p>`,
+                slideshow: ({ value }) => `<p>${value}</p>`,
+                video: ({ value }) => `<p>${value}</p>`,
+                audio: ({ value }) => `<p>${value}</p>`,
+                arbitraryEmbed: ({ value }) => `<p>${value}</p>`
+            }
+        }
+    })
+}
+
+export const toPlainText = (blocks: PortableTextBlock[]) => {
+    return blocks
+        .map(block => {
+            if (block._type !== 'block' || !block.children) {
+                return ''
+            }
+            return block.children.map(child => child.text).join('')
+        })
+        .join('\n\n')
 }
 
 const builder = imageUrlBuilder(client)
 
 export const urlFor = (source: any) => builder.image(source)
-
-const serializers = {
-    marks: {
-        link: props =>
-            h(
-                'a',
-                { target: '_blank', rel: 'noreferrer', href: props.mark.href },
-                props.children
-            )
-    },
-    types: {
-        block: props => {
-            const style = props.node.style || 'normal'
-            // console.dir(props)
-            return style === 'blockquote'
-                ? h('blockquote', {}, props.children)
-                : h('p', { className: style }, props.children)
-
-        },
-        imageGroup: props => h('p', props.children),
-        slideshow: props => h('p', props.children),
-        video: props => h('p', props.children),
-        audio: props => h('p', props.children),
-        arbitraryEmbed: props => h('p', props.children)
-    }
-}
 
 export const loadData = async (query: string, params: any) => {
     try {
